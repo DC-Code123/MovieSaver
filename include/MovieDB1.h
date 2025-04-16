2760977344dc465e25045a2312e8423cb5d94488
DC-Code123 <Distinctcode123@gmail.com>
2025-04-16 22:55:07 +0200

    Writing code initially

---
+++ include/MovieDB1.h
@@ -0,0 +1,17 @@
+#ifndef MOVIEDB_H
+#define MOVIEDB_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+struct M_info {
+  string title;
+  int year;
+  double price;
+};
+
+
+
+#endif
